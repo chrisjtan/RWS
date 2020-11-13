2fecd437ae88044acbe2128ca7421ffa91b655fc
Juntao Tan <hust.jtt@gmail.com>
2020-11-13 01:13:21 -0500

    add graph construction

--- scripts/CORe50/graph_construction/compute_distance_matrix.sh
+++ scripts/CORe50/graph_construction/compute_distance_matrix.sh
@@ -1,4 +1,6 @@
 python compute_distance_matrix.py\
   --feature_path '../../../projection_datasets/CORe50/features.pickle'\
   --info_path '../../../projection_datasets/CORe50/infos.pickle'\
-  --global_K 300
+  --global_K 300\
+  --local_K 10\
+  --lambda_ 0.1
