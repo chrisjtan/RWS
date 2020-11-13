d08bf7df8d2888434cc66569bcee286b416b4826
Juntao Tan <hust.jtt@gmail.com>
2020-11-13 16:26:38 -0500

    add graph building code

---
+++ scripts/CORe50/graph_construction/build_graph.sh
@@ -0,0 +1,6 @@
+python build_graph.py\
+  --feature_path '../../../projection_datasets/CORe50/features.pickle'\
+  --info_path '../../../projection_datasets/CORe50/infos.pickle'\
+  --global_K 300\
+  --local_K 10\
+  --lambda_ 0.1
