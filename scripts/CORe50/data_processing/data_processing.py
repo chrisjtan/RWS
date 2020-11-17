9745ea799afee71ae4cdb6f7cb3e0907165fff34
Juntao Tan <hust.jtt@gmail.com>
2020-11-17 00:54:45 -0500

    add data processing code, not finished yet

--- scripts/CORe50/data_processing/data_processing.py
+++ scripts/CORe50/data_processing/data_processing.py
@@ -1,4 +1,23 @@
+import numpy as np
+import cv2
+import os
 import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--data_path', type=str, help="CORe50 root path")
+args = parser.parse_args()
+
+if not os.path.exists("../../../projection_datasets"):
+    os.mkdir("../../../projection_datasets")
+
+if not os.path.exists("../../../projection_datasets/CORe50_2"):
+    os.mkdir("../../../projection_datasets/CORe50_2")
+    
+
+info_list = []  # [frame_num, tracking_sequence, category_label]
+feature_list = []
+
+frame_num = 0
+sequence_num = 0
+for tracking_folder in sorted(os.listdir(args.data_path), key=lambda f: int(''.join(filter(str.isdigit, f)))):
+    print(tracking_folder)
---
+++ scripts/CORe50/data_processing/data_processing.sh
@@ -0,0 +1,2 @@
+python data_processing.py\
+  --data_path "/home/jt/Datasets/core50_128x128/s1"
