adb3f5447dbfe7f4f6cc671e96610199a87bb3c9
Juntao Tan <hust.jtt@gmail.com>
2021-04-02 17:31:33 -0400

    delete files

--- scripts/CORe50/graph_construction/compute_distance_matrix.sh
+++
@@ -1,6 +0,0 @@
-python compute_distance_matrix.py\
-  --feature_path '../../../projection_datasets/CORe50/features.pickle'\
-  --info_path '../../../projection_datasets/CORe50/infos.pickle'\
-  --global_K 300\
-  --local_K 10\
-  --lambda_ 0.1
